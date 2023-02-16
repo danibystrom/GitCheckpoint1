2a370048d80206824e5f554ff18aaaa3f028fd75
danibystrom <124156950+danibystrom@users.noreply.github.com>
2023-02-16 15:51:11 +0100

    Add files via upload

---
+++ CheckTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCheckpoint1
+{
+    internal class CheckTest
+    {
+        public void testMessage()
+        {
+           public string message = "Task 2 checked.";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GitCheckpoint1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the Git checkpint task 1.1");
+            CheckTestHelpers.TestMessage();
+        }
+    }
+}
